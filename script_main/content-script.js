ebb9ce5f02dad2ef3499e8f1d7f405c0694abc99
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2023-06-11 18:54:34 +0900

    약간 개선

--- script_main/content-script.js
+++ script_main/content-script.js
@@ -3,21 +3,19 @@ $(document).ready(function() { // 베라, 핫딜 제외
   window.location.href.indexOf("arca.live/b/live") === -1 &&
   window.location.href.indexOf("arca.live/b/hotdeal") === -1
   ) {
-    const listRateCheck = () => {
-      // 베스트 라이브, 핫딜은 좀 다름
+    const listRateCheck = () => { // 추천 체크
       const postList = document.querySelectorAll('a.vrow.column'); // 게시글 리스트
       for (let i = 0; i< postList.length; i++) {
         if (postList[i].querySelector('.vcol.col-rate') && postList[i].className == 'vrow column') {
           let postList_rate = postList[i].querySelector('.vcol.col-rate').innerText; // 추천
           if (postList_rate < 0) {
             postList[i].style.opacity = '0.1';
-          } else if (postList_rate > 1) {
+          } else if (postList_rate > 3) {
             postList[i].style.fontWeight = 'bold';
           }
         }
       }
     }
-    listRateCheck();
 
     const createReloader = $(`<li><span id="Arcaplus_reloader" class=""></span></li>`);
     createReloader.prependTo($('.nav-control'));
@@ -38,17 +36,20 @@ $(document).ready(function() { // 베라, 핫딜 제외
         reloadTimer = setInterval(reloadFunc, 5000);
       }
     })
-    const reloadFunc = () => {
+    const reloadFunc = async () => {
       if (localStorage.getItem("isReload") == "true") {
-        $.get(document.URL, function(data) {
-          const parser = new DOMParser();
-          let doc = parser.parseFromString(data, "text/html");
-          $('.list-table.table').replaceWith(doc.querySelector(".list-table.table"));
+        $.get(window.location.href, function(data) {
+          $('.list-table.table')
+            .replaceWith(new DOMParser()
+              .parseFromString(data, "text/html")
+              .querySelector(".list-table.table")
+            );
           listRateCheck();
         })
         console.log('리로드')
       }
     };
     reloadTimer = setInterval(reloadFunc, 5000);
+    listRateCheck();
   }
 })

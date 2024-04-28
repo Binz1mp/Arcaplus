dc2bb6bd9c071a7deb5c6d32c07d09e1afd96c79
Binz1mp <binieon@naver.com>
2024-04-28 19:13:13 +0900

    팡고 주거라

--- src/js_inject/adBlock.js
+++ src/js_inject/adBlock.js
@@ -2,13 +2,17 @@ const $ = require( "jquery" );
 $(document).ready(function() {
   let interval = setInterval(() => {
     if ($("#svQazR5NHC3xCQr3 > ins")?.hasClass('adsbygoogle') === true) {
-      $("#svQazR5NHC3xCQr3 > ins").parent().css('display', 'none');
+      $("#svQazR5NHC3xCQr3").attr('style', 'display: none;');
       clearInterval(interval);
     } else if ($("#svQazR5NHC3xCQr3 > a.a-badge.sponsored").text() === "SPONSOR") {
-      $("#svQazR5NHC3xCQr3 > ins").parent().css('display', 'none');
+      $("#svQazR5NHC3xCQr3").attr('style', 'display: none;');
       clearInterval(interval);
+    } else if ($("#svQazR5NHC3xCQr3 > div.vm-placement").length > 0) {
+      console.log('$("#svQazR5NHC3xCQr3 > div.vm-placement") ===', $("#svQazR5NHC3xCQr3 > div.vm-placement"))
+      $("#svQazR5NHC3xCQr3").attr('style', 'display: none;');
     }
-  }, 500);
+    $(".sticky-container > .ad").attr('style', 'display: none;');
+  }, 2500);
 
   setTimeout(() => {
     if ($("#svQazR5NHC3xCQr3 > ins")?.hasClass('adsbygoogle') !== true) {

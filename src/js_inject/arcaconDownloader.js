caeafcc7af66a0614b0e74ced4c38923e71a493e
Binz1mp <binieon@naver.com>
2024-06-11 22:08:06 +0900

    v1.2.11 - 아카콘 다운로더 먹통되던 문제 해결

--- src/js_inject/arcaconDownloader.js
+++ src/js_inject/arcaconDownloader.js
@@ -4,7 +4,6 @@ require("file-saver");
 
 const arcaPostURLRegex1 = /^https:\/\/arca\.live\/b\/[^\/]+\/\d+\/\d+$/; // 아카라이브 게시글 url 정규식
 const arcaPostURLRegex2 = /^https:\/\/arca\.live\/b\/[^\/]+\/\d+$/; // 아카라이브 게시글 url 정규식
-const imageFormatRegex = /\.(jpg|jpeg|png|gif|webp)(\?.*)?$/i; // 아카라이브 아카콘 파일포멧 정규식
 let arcaconTitleName = '최근 사용'; // 아카콘별 타이틀 아이디 - 아이디에 따른 타이틀 네임이 아카콘 폴더에 저장될 예정
 let arcaconsURLArray = []; // 아카콘 주소가 저장된 배열
 let downloadCount = 0; // 다운로드 카운트
@@ -73,8 +72,9 @@ $(document).ready(function() {
   ) {
     const arcaconFormContainer = $('.reply-form-button-container'); // 댓글 컨테이너
     const arcaconButton = $('.reply-form-arcacon-button.btn-namlacon'); // 아카콘 버튼
-    const arcaconWrapper = $('.namlacon'); // 아카콘 버튼 누르면 보이는 영역
-    const arcaconTitle = $('.thumbnails'); // 아카콘별 타이틀 이미지 - 아이디에 따른 타이틀 네임이 나올 예정
+    const arcaconWrapper = $('.arcaconPicker'); // 아카콘 버튼 누르면 보이는 영역
+    let arcaconPackageId = 0; // 아카콘 타이틀별 패키지 아이디
+    let arcaconImageContainer = $('.package-wrap[data-package-id="' + arcaconPackageId + '"]'); // 아카콘 이미지 컨테이너
 
     // 아카콘 버튼을 클릭하면 아카콘 다운로드 버튼의 표시 여부를 변경한다.
     arcaconButton.click(function (e) { 
@@ -96,43 +96,22 @@ $(document).ready(function() {
     `);
     const arcaconDownloadButton = $('.reply-form-voice-button.btn-arcaconDownload'); // 아카콘 다운로드 버튼
 
-    // 아카콘별 타이틀 이미지를 클릭하면 해당 이미지의 아이디를 통해서 아카콘 이름을 긁어온다.
-    arcaconTitle.on("click", "img", function (e) {
-      let arcaconURL = `https://arca.live/e/${$(e.target).data('id')}`;
-      // console.log('아이디 ===', $(e.target).data('id'));
-      if ($(e.target).data('id') === 0) {
-        // console.log('타이틀 ===', '최근 사용');
-        arcaconTitleName = '최근 사용';
-      } else {
-        fetch(arcaconURL)
-          .then(response => {
-            if (!response.ok) {}
-            return response.text();
-          })
-          .then(html => {
-            let title = new DOMParser().parseFromString(html, 'text/html').querySelector('title').textContent;
-            // console.log('타이틀 ===', title);
-            arcaconTitleName = title;
-          })
-          .catch(error => {
-            console.error('몬가 문제가 생겨슴:', error);
-          });
-      }
-    });
-
     // 아카콘 다운로드 버튼을 클릭하면 현재 포커스된 아카콘을 전부 다운로드한다.
     arcaconDownloadButton.click(async function (e) {
       e.preventDefault();
+      arcaconTitleName = $('.arcaconPicker .package-item.focused').data('packageName');
+      arcaconPackageId = $('.arcaconPicker .package-item.focused').data('packageId');
+      arcaconImageContainer = $('.package-wrap[data-package-id="' + arcaconPackageId + '"]');
+      console.log(arcaconTitleName, arcaconPackageId, arcaconImageContainer);
       if (!isDownloading) {
         arcaconsURLArray = [];
-        const arcacons = $('.emoticons');
-        arcacons.children().each(function (index, element) {
-          // element == this
-          // console.log(index, element.src);
-          arcaconsURLArray.push({
-            url: element.src,
-          });
-          
+        arcaconImageContainer.find('.thumbnails').children('.thumbnail-wrap').each(function() {
+          var imgSrc = $(this).find('img.thumbnail').attr('src');
+          if (imgSrc) {
+            arcaconsURLArray.push({
+              url: imgSrc,
+            });
+          }
         });
         saveFilesAsZip(arcaconsURLArray);
       } else {
